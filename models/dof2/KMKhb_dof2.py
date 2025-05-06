46be4131496c5c1754dc4a175c36adc4d59dc238
AngelosGuan <angelosguan@gmail.com>
2025-05-05 21:37:39 -0400

    kdelta KMKhb fix

--- models/dof2/KMKhb_dof2.py
+++ models/dof2/KMKhb_dof2.py
@@ -6,6 +6,7 @@ from models.dof2 import dynamics
 ########################################################################
 k_a = 0.7
 k_b = 1.3
+k_delta = 0.3
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 ########################################################################
 # rewritten model class to accept both batched an unbatch input
