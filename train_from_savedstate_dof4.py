4815b461eb3d8279625a1675d4a1969a92e0919b
AngelosGuan <angelosguan@gmail.com>
2025-09-22 02:47:37 -0400

    typo

--- train_from_savedstate_dof4.py
+++ train_from_savedstate_dof4.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     adam = torch.optim.AdamW(model.parameters(), lr=config.lr_adam, weight_decay=config.l2_regu_adam)
 
     # load checkpoint
-    total_epoch, X = plot.load_checkpoint(model, adam, storage_path, device=device)
+    total_epoch, X = plot.load_checkpoint(model, adam, STORAGE_PATH, device=device)
     
     # train with custom settings
     ############################
