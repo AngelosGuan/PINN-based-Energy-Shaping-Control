63d11fe63c19bf7ce105c73c484c5cc942b9d53c
AngelosGuan <angelosguan@gmail.com>
2026-03-14 02:32:16 -0400

    checking output

---
+++ analyze.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32G
+#SBATCH --gpus=1
+#SBATCH --constraint=gpu_a100_40gb|gpu_a100_80gb
+# Bash script: main_512_JMJ.sh
+
+# --- deterministic math + single-threaded BLAS/OMP ---
+export CUBLAS_WORKSPACE_CONFIG=":4096:8"   # or ":16:8"
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export PYTHONHASHSEED="0"
+
+
+python analyze_residual.py --model_name dof4_JMJ_512 &
+wait
